fd2306aa8a7507c2ae17d5bfcfdfb694537e9af5
jp-rad <jp.rad@outlook.jp>
2023-03-24 22:28:04 +0900

    version: 0.0.4

--- src/components/MicrobitIoPin.tsx
+++ src/components/MicrobitIoPin.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { WbBoundCallback } from '@weblueth/statemachine';
 import { WbxCustomEventCallback, WbxServiceProps, WbxServices } from '@weblueth/react';
-import { IoPinService, PinData } from 'microbit-web-bluetooth/types/services/io-pin';
+import { AD, IO, IoPinService, PinData } from 'microbit-web-bluetooth/types/services/io-pin';
 import { Services } from 'microbit-web-bluetooth';
 
 interface Props extends WbxServiceProps<IoPinService> {
@@ -9,11 +9,20 @@ interface Props extends WbxServiceProps<IoPinService> {
      * Pin data changed event
      */
     onPinDataChanged?: WbxCustomEventCallback<PinData[]>;
+    /**
+     * Set pin analogue/digital configuration
+     */
+    adConfiguration: AD[];
+    /**
+     * Set pin input/output configuration
+     */
+    ioConfiguration: IO[];
 }
 
 const pindatachanged = "pindatachanged";
 
 export function MicrobitIoPin(props: Props) {
+    const [service, setService] = useState<IoPinService | undefined>(undefined);
 
     const onServicesBound: WbBoundCallback<Services> = bound => {
         const target = bound.target.ioPinService;
@@ -22,16 +31,30 @@ export function MicrobitIoPin(props: Props) {
                 if (props.onPinDataChanged) {
                     target.addEventListener(pindatachanged, props.onPinDataChanged)
                 }
+                setService(target);
             } else {
                 if (props.onPinDataChanged) {
                     target.removeEventListener(pindatachanged, props.onPinDataChanged)
                 }
+                setService(undefined);
             }
             if (props.onServiceBound) {
                 props.onServiceBound({ ...bound, target });
             }
         }
     };
+
+    useEffect(() => {
+        if (service && props.adConfiguration) {
+            service.setAdConfiguration(props.adConfiguration);
+        }
+    }, [service, props.adConfiguration]);
+    
+    useEffect(() => {
+        if (service && props.ioConfiguration) {
+            service.setIoConfiguration(props.ioConfiguration);
+        }
+    }, [service, props.ioConfiguration]);
 
     return (
         <WbxServices onServicesBound={onServicesBound} />
